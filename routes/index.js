5ea52a052f64b824333673de603b287d261830aa
Iván Guerrero <theiviboss083@gmail.com>
2025-04-14 19:05:28 +0200

    proyectos empezado + revision requisitos (test pendiente para proyectos)

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,7 @@ router.post('/mail', validatorMail, send)
 
 router.use('/user', authRoutes);
 router.use('/client', require('./client'));
+router.use('/project', require('./project'));
+
 
 module.exports = router;
---
+++ validators/projects.js
@@ -0,0 +1,15 @@
+const { check } = require("express-validator");
+
+const projectValidator = [
+  check("name").notEmpty().withMessage("El nombre del proyecto es obligatorio."),
+  check("projectCode").notEmpty().withMessage("El projectCode es obligatorio."),
+  check("code").notEmpty().withMessage("El código interno del proyecto es obligatorio."),
+  check("clientId").notEmpty().withMessage("El ID del cliente es obligatorio."),
+  check("address.street").notEmpty().withMessage("La calle es obligatoria."),
+  check("address.number").isInt({ min: 1 }).withMessage("El número debe ser entero positivo."),
+  check("address.postal").isInt().withMessage("El código postal debe ser numérico."),
+  check("address.city").notEmpty().withMessage("La ciudad es obligatoria."),
+  check("address.province").notEmpty().withMessage("La provincia es obligatoria.")
+];
+
+module.exports = { projectValidator };
